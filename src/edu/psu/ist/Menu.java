20c8feb09522f3ae766daeb334db24c30cd08eac
mvc5630 <51428005+mvc5630@users.noreply.github.com>
2019-06-15 08:01:29 -0400

    Add files via upload

---
+++ src/edu/psu/ist/Menu.java
@@ -0,0 +1,34 @@
+package edu.psu.ist;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+    //Class Level Variables - Protect the data
+    private int menuId;
+    private String menuItem;
+    private double menuPrice;
+
+    //Constructor Method
+    public Menu(int _menuId, String _menuItem, double _menuPrice){
+        this.menuId = _menuId;
+        this.menuItem = _menuItem;
+        this.menuPrice = _menuPrice;
+    }
+
+    //Setters and Getters
+    public int getmenuId() { return menuId; }
+    public void setmenuId(int _menuId) {this.menuId = _menuId;}
+
+    public String getmenuItem() { return menuItem; }
+    public void setmenuItem(String _menuItem) {this.menuItem = _menuItem;}
+
+    public double getmenuPrice() { return menuPrice; }
+    public void setmenuPrice(Double _menuPrice) {this.menuPrice = _menuPrice;}
+
+    public static void listMenu(ArrayList<Menu> mList){
+        for (Menu menu: mList){
+            System.out.println(menu.getmenuItem());
+        }
+    }
+}
---
+++ src/edu/psu/ist/Order.java
@@ -0,0 +1,27 @@
+package edu.psu.ist;
+
+import java.util.ArrayList;
+
+public class Order {
+    //Class Level Variables - Protect the data
+    private int orderId;
+    private Customer cust;
+    private ArrayList<Menu> menuItem;
+
+    //Constructor Method
+    public Order(int _orderId){
+        this.orderId = _orderId;
+    }
+
+    //Setters and Getters
+    public int getorderId() { return orderId; }
+    public void setorderId(int _orderId) {this.orderId = _orderId;}
+
+    public static void printOrder(ArrayList<Order> oList){
+        for (Order order: oList){
+            System.out.println("Order Id:" + order.getorderId());
+
+        }
+    }
+
+}
